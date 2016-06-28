01bff20c7d31b0d4bb6108270b2e6e03a1935e3f
Alisa Hubic <alisa@Brus-MacBook-Air.local>
2016-06-28 16:31:07 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/alisa/teamdataautomation/LoginNegativeTests.java
@@ -0,0 +1,37 @@
+package com.example.alisa.teamdataautomation;
+
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.Until;
+import android.test.InstrumentationTestCase;
+
+
+/**
+ * Created by alisa on 6/28/16.
+ */
+public class LoginNegativeTests extends InstrumentationTestCase {
+
+    private UiDevice device;
+    static final String applicationName = "TeamData";
+
+    @Override
+    public void setUp() throws Exception {
+        device = UiDevice.getInstance(getInstrumentation());
+        device.pressHome();
+
+        device.wait(Until.hasObject(By.text(applicationName)), 5000);
+        UiObject2 DataVault = device.findObject(By.text(applicationName));
+        DataVault.click();
+    }
+
+
+    public void testLoginWithIncorrectUsername() throws Exception {
+        
+    }
+
+    public void testLoginWithIncorrectPassword() throws Exception {
+
+    }
+
+}
